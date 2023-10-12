758f1e3e72d5f1c7f681b616a0db1b41e1fe2802
Augustin Serban <gusti@allforads.com>
2023-10-12 15:24:24 +0300

    v11

--- confucius.js
+++ confucius.js
@@ -2,12 +2,11 @@ let macrou="bogat in grasimi bune"
 DynamicsCompressorNodedssd
 TextDecoderStream
 
-<<<<<<< HEAD
 din MediaDeviceInfo
 fdfdf
 fdfdf
-=======
 Ar trebui sa fie doar in v4 ram
 Ar trebui sa fie doar in v4 ram 2
 Aici merge in v4ram3
->>>>>>> v4-ram
+
+ver 11
